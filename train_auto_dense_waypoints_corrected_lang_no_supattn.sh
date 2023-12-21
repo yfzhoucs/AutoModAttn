29b346d685d2eb16b4775c0886c221b43997a725
Yifan Zhou <yfzhou.cs@gmail.com>
2023-12-20 17:49:36 -0700

    Added ablation study: no sup attn

---
+++ train_auto_dense_waypoints_corrected_lang_no_supattn.sh
@@ -0,0 +1,5 @@
+export CUDA_VISIBLE_DEVICES=0
+python main_real_robot_auto_dense_waypoints_corrected_lang_no_supattn.py \
+  /data/yzhou298/dataset/auto_modattn/ur5_table_top_manip_2_cam \
+  /data/yzhou298/dataset/auto_modattn/ur5_table_top_manip_2_cam_val \
+  /data/yzhou298/ckpts/AutoModAttn
